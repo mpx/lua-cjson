a6f950fa3b1f5d2b50a405cca5a74000dcf3dd16
Ignacio Burgueño <ignaciob@inconcertcc.com>
2013-04-09 12:16:20 -0300

    Allows to encode empty tables as arrays.
    An option is added to encode empty Lua tables to arrays. The option is
    "encode_empty_table_as_array" and is set like this:
    
       cjson.encode_empty_table_as_array("on")
       assert( cjson.encode{} == "[]" )
    
    By default, the option is off.
    
    refs #2347

--- lua_cjson.c
+++ lua_cjson.c
@@ -82,6 +82,7 @@
 #define DEFAULT_DECODE_INVALID_NUMBERS 1
 #define DEFAULT_ENCODE_KEEP_BUFFER 1
 #define DEFAULT_ENCODE_NUMBER_PRECISION 14
+#define DEFAULT_ENCODE_EMPTY_TABLE_AS_ARRAY 0 /* 0 table, 1 array */
 
 #ifdef DISABLE_INVALID_NUMBERS
 #undef DEFAULT_DECODE_INVALID_NUMBERS
@@ -138,6 +139,7 @@ typedef struct {
     int encode_invalid_numbers;     /* 2 => Encode as "null" */
     int encode_number_precision;
     int encode_keep_buffer;
+    int encode_empty_table_as_array;
 
     int decode_invalid_numbers;
     int decode_max_depth;
@@ -335,6 +337,13 @@ static int json_cfg_encode_keep_buffer(lua_State *l)
     return 1;
 }
 
+/* Configures whether empty tables will be encoded as an object or an array */
+static int json_cfg_encode_empty_tables_as_array(lua_State *l)
+{
+    json_config_t *cfg = json_arg_init(l, 1);
+    return json_enum_option(l, 1, &cfg->encode_empty_table_as_array, NULL, 1);
+}
+
 #if defined(DISABLE_INVALID_NUMBERS) && !defined(USE_INTERNAL_FPCONV)
 void json_verify_invalid_number_setting(lua_State *l, int *setting)
 {
@@ -404,6 +413,7 @@ static void json_create_config(lua_State *l)
     cfg->decode_invalid_numbers = DEFAULT_DECODE_INVALID_NUMBERS;
     cfg->encode_keep_buffer = DEFAULT_ENCODE_KEEP_BUFFER;
     cfg->encode_number_precision = DEFAULT_ENCODE_NUMBER_PRECISION;
+    cfg->encode_empty_table_as_array = DEFAULT_ENCODE_EMPTY_TABLE_AS_ARRAY;
 
 #if DEFAULT_ENCODE_KEEP_BUFFER > 0
     strbuf_init(&cfg->encode_buf, 0);
@@ -699,7 +709,7 @@ static void json_append_data(lua_State *l, json_config_t *cfg,
         current_depth++;
         json_check_encode_depth(l, cfg, current_depth, json);
         len = lua_array_length(l, cfg, json);
-        if (len > 0)
+        if (len > 0 || (cfg->encode_empty_table_as_array && len == 0))
             json_append_array(l, cfg, current_depth, json, len);
         else
             json_append_object(l, cfg, current_depth, json);
@@ -1373,6 +1383,7 @@ static int lua_cjson_new(lua_State *l)
         { "encode_keep_buffer", json_cfg_encode_keep_buffer },
         { "encode_invalid_numbers", json_cfg_encode_invalid_numbers },
         { "decode_invalid_numbers", json_cfg_decode_invalid_numbers },
+        { "encode_empty_table_as_array", json_cfg_encode_empty_tables_as_array },
         { "new", lua_cjson_new },
         { NULL, NULL }
     };
